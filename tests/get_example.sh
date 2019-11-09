75bfa03daaaf1f3c245884c5ed440d0014504d16
spookypeanut <github@spookypeanut.co.uk>
2019-11-09 22:12:25 +0000

    Put it in a tmp folder that we can ignore

--- tests/get_example.sh
+++ tests/get_example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -o $0.json 'https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/SSSI_England/FeatureServer/0/query?where=OBJECTID%3D3072&outFields=OBJECTID,SSSI_NAME,GIS_FILE&outSR=4326&f=json'
+curl -o tmp/$0.json 'https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/SSSI_England/FeatureServer/0/query?where=OBJECTID%3D3072&outFields=OBJECTID,SSSI_NAME,GIS_FILE&outSR=4326&f=json'
