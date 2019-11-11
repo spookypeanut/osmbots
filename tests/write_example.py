932caed4230d62926e4c988b37bc5678cc7ed706
spookypeanut <github@spookypeanut.co.uk>
2019-11-11 23:00:00 +0000

    Make a building

--- tests/write_example.py
+++ tests/write_example.py
@@ -5,29 +5,31 @@ LAT_MAX = 51.82129
 LONG_MIN = -0.23969
 LONG_MAX = -0.23852
 
+OUTER_COORDS = [(LAT_MIN, LONG_MIN),
+                (LAT_MAX, LONG_MIN),
+                (LAT_MAX, LONG_MAX),
+                (LAT_MIN, LONG_MAX)]
+
+# See the result here:
+# https://master.apis.dev.openstreetmap.org/#map=17/51.82032/-0.23782
 OSM_URL = "https://master.apis.dev.openstreetmap.org/"
 
 
-def get_credentials():
-    """ Get credentials from a file in the current directory, in the form:
-
-        username
-        P4$$W0rD
-
-    """
-    with open("credentials.txt", "r") as cred_file:
-        lines = cred_file.readlines()
-        username = lines[0].strip()
-        password = lines[1].strip()
-    return {"username": username, "password": password}
-
-
 def main():
-    credentials = get_credentials()
-    api = osmapi.OsmApi(api=OSM_URL, **credentials)
+    api = osmapi.OsmApi(api=OSM_URL, passwordfile="credentials.txt")
     api.ChangesetCreate({u"comment": u"Test of automated edit"})
     tags = {"name": "Test node", "natural": "peak"}
     print(api.NodeCreate({u"lon": LONG_MIN, u"lat": LAT_MIN, u"tag": tags}))
+    nodes = []
+    first = None
+    for lat, lon in OUTER_COORDS:
+        nodes.append(api.NodeCreate({"lon": lon, "lat": lat})["id"])
+        if first is None:
+            first = nodes[-1]
+    nodes.append(first)
+    tags = {"name": "Test way", "building": "yes"}
+    api.WayCreate({"nd": nodes, "tag": tags})
+
     api.ChangesetClose()
 
 
