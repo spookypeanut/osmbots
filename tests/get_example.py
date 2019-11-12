a246ebf82418095824fc400493f5c20abe773c1d
spookypeanut <github@spookypeanut.co.uk>
2019-11-12 20:33:33 +0000

    Get all fields (we might need them!)

--- tests/get_example.py
+++ tests/get_example.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
-import requests
 import json
 
-params = {"where": "OBJECTID=3072",
-          "outFields": "OBJECTID,SSSI_NAME,GIS_FILE",
-          "outSR": "4326",
-          "f": "json"}
-url = 'https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/SSSI_England/FeatureServer/0/query'
-r = requests.get(url, params=params)
-json_string = json.dumps(r.json(), sort_keys=True, indent=4)
-with open("tmp/get_example.py.json", "w") as f:
+import requests
 
-    f.write(json_string)
+QUERY_URL = ('https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/'
+             'services/SSSI_England/FeatureServer/0/query')
+OUTFIELDS = ["OBJECTID", "SSSI_NAME", "SSSI_AREA", "EASTING", "NORTHING",
+             "LATITUDE", "LONGITUDE", "REFERENCE", "STATUS", "GID", "ENSISID",
+             "GIS_FILE", "AREA", "EASTING0", "NORTHING0", "GIS_DATE",
+             "VERSION"]
+def main():
+    params = {"where": "OBJECTID=3072",
+              "outFields": ",".join(OUTFIELDS),
+              "outSR": "4326",
+              "f": "json"}
+    result = requests.get(QUERY_URL, params=params)
+    json_string = json.dumps(result.json(), sort_keys=True, indent=4)
+    with open("tmp/get_example.py.json", "w") as jsonfile:
+        jsonfile.write(json_string)
+
+
+if __name__ == "__main__":
+    main()
